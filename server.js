59df834f8dfe86781df89c3c35453eb8312d5594
Dennis Brandt <dennis.brandt@mybit.nl>
2015-11-18 11:43:31 +0100

    Added random replies when just calling the bot by it's name

--- server.js
+++ server.js
@@ -40,7 +40,18 @@
           channel.send('Remnants\' last known realm-rank on WoWProgress is *' + JSON.parse(body).realm_rank + '*');
         });
       } else {
-        channel.send('I don\'t get it.');
+        var responses = [
+          'I don\'t get it.',
+          '*¯\\(º_o)/¯*',
+          'Yes?',
+          'Hello, <@' + message.user + '>',
+          ':wave:',
+          ':robot_face:'
+        ];
+
+        var response = responses[Math.floor(Math.random() * responses.length)];
+
+        channel.send(response);
       }
     } else {
       if (hasWord(message, '(╯°□°）╯︵ ┻━┻')) {
